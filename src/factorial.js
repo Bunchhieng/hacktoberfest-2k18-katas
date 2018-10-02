4470db07a85519bc8e1d06e541ac71e385a0c163
Bunchhieng Soth <bsoth@tracelink.com>
2018-10-02 12:10:00 -0400

    Added factorial

--- src/factorial.js
+++ src/factorial.js
@@ -1,3 +1,7 @@
-export const factorial = () => {
+export const factorial = n => {
+  if (n < 0) throw new Error("Must provide positive number!!!");
   // TO IMPLEMENT IN ANOTHER PR
+  if (n === 0 || n === 1) {
+    return 1;
+  } else return n * factorial(n - 1);
 };
--- src/factorial.test.js
+++ src/factorial.test.js
@@ -1,15 +1,27 @@
-import { factorial } from './factorial';
+import { factorial } from "./factorial";
 
-describe('factorial', () => {
-  it('returns correct value when supplied 0', () => {
+describe("factorial", () => {
+  it("returns throw exception when supplied negative number", () => {
+    try {
+      expect(factorial(-1)).toThrowError(Error);
+    } catch (e) {
+      expect(e.message).toBe("Must provide positive number!!!");
+    }
+  });
+
+  it("returns correct value when supplied 0", () => {
     expect(factorial(0)).toEqual(1);
   });
 
-  it('returns correct value when supplied integer (5)', () => {
+  it("returns correct value when supplied 1", () => {
+    expect(factorial(1)).toEqual(1);
+  });
+
+  it("returns correct value when supplied integer (5)", () => {
     expect(factorial(5)).toEqual(120);
   });
 
-  it('returns correct value when supplied integer (10)', () => {
+  it("returns correct value when supplied integer (10)", () => {
     expect(factorial(10)).toEqual(3628800);
   });
-})
+});
